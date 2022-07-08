3686b33b4094e35711fd57eb82727739c5160f63
Manoj Gautam <nix1947@users.noreply.github.com>
2022-07-08 07:59:16 +0545

    RSA

---
+++ javaAlgo/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String cipher = Util.encryption("Hello world");
+        System.out.println("CipherText is" + cipher);
+    }
+
+}
---
+++ javaAlgo/java/Util.java
@@ -0,0 +1,13 @@
+public class Util {
+
+    public static String encryption(String message) {
+        for (int i = 0; i <= message.length(); i++) {
+
+        }
+           
+    }
+
+    public static String decryption(String cipher) {
+        return "THISISPLAINTEXT";
+    }
+}
